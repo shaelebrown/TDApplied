9a72676dd5fdf1ee1e58df10eb48516867205877
Shael Brown <shaelebrown@gmail.com>
2021-12-24 11:11:03 -0500

    Adding full package documentation.

---
+++ R/TDAInference.R
@@ -0,0 +1,12 @@
+#' Statistical Inference for Persistence Diagrams
+#'
+#' This package provides the functionality to infer when
+#' there are topological differences between an arbitrary
+#' number of groups of persistence diagrams, and to quickly
+#' compute distances between persistence diagrams.
+#'
+#' @useDynLib TDAInference
+#' @import TDA, TDAstats, doParallel, clue, rdist
+#' @name TDAInference
+#' @docType package
+NULL
