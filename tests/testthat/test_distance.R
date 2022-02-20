03035bfa70cbe85fa54697948e25a276acd146db
Shael Brown <shaelebrown@gmail.com>
2022-02-19 21:51:48 -0500

    Added test folders and files.

---
+++ tests/testthat/test_distance.R
@@ -0,0 +1,20 @@
+context("Test distances between diagrams")
+library(TDAInference)
+
+test_that("diagram_distance detects incorrect parameters correctly",{
+  
+  # do
+  
+})
+
+test_that("diagram_distance can accept inputs from either TDA homology output or diagram_to_df function",{
+  
+  # do
+  
+})
+
+test_that("diagram_distance is working",{
+  
+  # do
+  
+})
---
+++ tests/testthat/test_inference.R
@@ -0,0 +1,14 @@
+context("Test inferences of persistence diagrams")
+library(TDAInference)
+
+test_that("permutation_test detects incorrect parameters correctly",{
+  
+  # do
+  
+})
+
+test_that("permutation_test is working",{
+  
+  # do, check for several types of datasets checking the test statistic, permutation values and p value
+  
+})
