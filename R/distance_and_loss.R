d6aeed288f09ff99a850873846ce48474396bf81
Shael Brown <shaelebrown@gmail.com>
2022-02-26 12:39:27 -0500

    Fixed parallel export error in distance function, and added permutation test example in package vignette.

--- R/distance_and_loss.R
+++ R/distance_and_loss.R
@@ -228,7 +228,7 @@ loss <- function(diagram_groups,dist_mats,dims,p,q,distance){
 
   # export necessary libraries and variables to cl
   parallel::clusterEvalQ(cl,c(library(clue),library(rdist)))
-  parallel::clusterExport(cl,c("diagram_distance","diagram_groups","dist_mats","dims","combinations","p"),envir = environment())
+  parallel::clusterExport(cl,c("check_diagram","diagram_distance","diagram_groups","dist_mats","dims","combinations","p"),envir = environment())
 
   # initialize return vector of statistics, one for each dimension
   statistics <- c()
