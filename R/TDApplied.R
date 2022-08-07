e6d6ca09a14b80deeb42506f529fcede1e85131e
JIB account <jibaccount@Jasons-MacBook-Pro.local>
2022-08-07 10:15:43 -0400

    Moved TDA and TDAstats to suggested, and changed package name to TDApplied.

--- R/TDApplied.R
+++ R/TDApplied.R
@@ -5,15 +5,14 @@
 #' power of TDA. The main tool of TDA is persistent homology, which computes a 
 #' shape descriptor of a dataset, called a persistence diagram. There are four
 #' goals of this package: (1) convert the output from the persistent homology
-#' calculations in the popular TDA package for R, 'TDA', into a data frame which can
+#' calculations in the two main R package for TDA into a data frame which can
 #' easily be used in personalized analyses, (2) provide a fast method for computing
 #' distances between persistence diagrams, (3) implement kernel machine learning
 #' method for persistence diagrams (currently kernel multidimensional scaling and
 #' kernel SVM), and (4) provide methods for inference on groups of persistence diagrams.
 #'
 #' @docType package
-#' @name TDAML
-#' @import TDA
+#' @name TDApplied
 #' @importFrom clue solve_LSAP
 #' @importFrom doParallel registerDoParallel
 #' @importFrom foreach foreach %dopar% %do%
